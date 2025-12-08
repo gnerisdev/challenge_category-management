a23edeca96a24c6c63c7620a52a74cf064cd1111
gnerisdev <gnerisdev@gmail.com>
2025-12-08 04:43:41 -0300

    fix: ajuste responsividade

--- src/resources/js/Pages/Categories.vue
+++ src/resources/js/Pages/Categories.vue
@@ -254,8 +254,8 @@ onMounted(() => {
 <template>
     <AppLayout>
         <section class="max-w-6xl mx-auto p-4 md:p-8">
-            <header class="flex items-center justify-between mb-6">
-                <h1 class="text-3xl font-bold text-gray-900">Categorias</h1>
+            <header class="flex wrap gap-3 items-center justify-between mb-6">
+                <h1 class="text-2xl md:text-2xl font-bold text-gray-900">Categorias</h1>
                 <ButtonDefault @click="openModal">Nova Categoria</ButtonDefault>
             </header>
 
